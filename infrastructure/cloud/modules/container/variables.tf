e417962a1c3a5759ab07d3d678f64c8600698172
Ronaldo Macapobre <rmacapob@gov.bc.ca>
2024-07-24 00:02:04 +0000

    Use env variables from root

--- infrastructure/cloud/modules/container/variables.tf
+++ infrastructure/cloud/modules/container/variables.tf
@@ -1,13 +1,10 @@
-
-
 variable "environment" {
   type        = string
   description = "The environment to deploy the application to"
-  default     = "dev"
 }
 
 variable "ecr_repository_name" {
   type        = string
   description = "Name of AWS ECR Repository"
-  default     = "aws-ecr-repository"
+  default     = "aws_ecr_repository"
 }

b848277825a364610eef6a411738fe59d11e477d
Ronaldo Macapobre <rmacapob@gov.bc.ca>
2024-07-25 20:38:03 +0000

    Fixed plan error

--- infrastructure/cloud/modules/networking/vpc.tf
+++ infrastructure/cloud/modules/networking/vpc.tf
@@ -23,7 +23,7 @@ resource "aws_route_table" "route_table" {
 }
 
 resource "aws_route_table_association" "route_table_association" {
-  subnet_id      = aws_subnet.subnet.id
+  subnet_id      = aws_subnet.subnet[*].id
   route_table_id = aws_route_table.route_table.id
 }
 

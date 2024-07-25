664bcb8d10eed87478fd2074826c26c580294002
Ronaldo Macapobre <rmacapob@gov.bc.ca>
2024-07-25 19:25:24 +0000

    Fixed renamed resource

--- infrastructure/cloud/modules/container/outputs.tf
+++ infrastructure/cloud/modules/container/outputs.tf
@@ -1,4 +1,4 @@
 output "ecr_url" {
   description = "The ECR URL."
-  value       = try(aws_ecr_repository.aws_ecr_repository.repository_url, "")
+  value       = try(aws_ecr_repository.ecr_repository.repository_url, "")
 }

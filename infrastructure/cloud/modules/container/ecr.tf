208c4544d868f19cb86af3f3efe7f0adc3c009cc
Ronaldo Macapobre <rmacapob@gov.bc.ca>
2024-07-26 22:57:07 +0000

    Added force_delete in ECR repo resource

--- infrastructure/cloud/modules/container/ecr.tf
+++ infrastructure/cloud/modules/container/ecr.tf
@@ -1,6 +1,7 @@
 resource "aws_ecr_repository" "ecr_repository" {
   name                 = "${var.app_name}-repo-${var.environment}"
   image_tag_mutability = "MUTABLE"
+  force_delete         = true
 
   image_scanning_configuration {
     scan_on_push = true

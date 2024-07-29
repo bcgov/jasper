1ca481bcae5f9073592e2e30b3fa61b94e7600cd
Ronaldo Macapobre <rmacapob@gov.bc.ca>
2024-07-29 16:08:18 +0000

    Fixed ecs web task role policy

--- infrastructure/cloud/modules/security/iam.tf
+++ infrastructure/cloud/modules/security/iam.tf
@@ -9,8 +9,22 @@ resource "aws_iam_role" "ecs_web_task_execution_role" {
         Principal = {
           Service = "ecs-tasks.amazonaws.com"
         }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "ecs_web_task_execution_policy" {
+  name = "${var.app_name}-ecs-web-task-execution-policy-${var.environment}"
+  role = aws_iam_role.ecs_web_task_execution_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
         Action = [
-          "sts:AssumeRole",
           "ecr:BatchGetImage",
           "ecr:GetDownloadUrlForLayer",
           "ecr:GetAuthorizationToken"
@@ -19,9 +33,3 @@ resource "aws_iam_role" "ecs_web_task_execution_role" {
     ]
   })
 }
-
-resource "aws_iam_role_policy_attachment" "ecs_web_task_execution_role_policy" {
-  role       = aws_iam_role.ecs_web_task_execution_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-}
-

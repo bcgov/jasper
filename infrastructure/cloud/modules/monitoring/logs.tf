11ff771ddf598238bc90e81e2d2c5a7ead3da73f
Ronaldo Macapobre <rmacapob@gov.bc.ca>
2024-08-26 22:38:34 +0000

    Removed retention days

--- infrastructure/cloud/modules/monitoring/logs.tf
+++ infrastructure/cloud/modules/monitoring/logs.tf
@@ -1,6 +1,4 @@
 resource "aws_cloudwatch_log_group" "ecs_web_td_log_group" {
-  name              = "/aws/ecs/${var.app_name}-ecs-web-td-log-group-${var.environment}"
-  retention_in_days = 90
-
+  name       = "/aws/ecs/${var.app_name}-ecs-web-td-log-group-${var.environment}"
   kms_key_id = var.kms_key_arn
 }

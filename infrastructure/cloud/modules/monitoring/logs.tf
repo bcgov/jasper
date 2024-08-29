80464992cb1ac98909dbf70bafb3a5e54d9d9fa7
Ronaldo Macapobre <rmacapob@gov.bc.ca>
2024-08-29 01:10:34 +0000

    Add retention days

--- infrastructure/cloud/modules/monitoring/logs.tf
+++ infrastructure/cloud/modules/monitoring/logs.tf
@@ -6,6 +6,8 @@ resource "aws_cloudwatch_log_group" "ecs_web_td_log_group" {
 }
 
 resource "aws_cloudwatch_log_group" "ecs_api_td_log_group" {
-  name       = "/aws/ecs/${var.app_name}-ecs-api-td-log-group-${var.environment}"
+  name              = "/aws/ecs/${var.app_name}-ecs-api-td-log-group-${var.environment}"
+  retention_in_days = 90
+
   kms_key_id = var.kms_key_arn
 }

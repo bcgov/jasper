8ca653fd6f7b911ee6125d44f63866d63280646c
Ronaldo Macapobre <rmacapob@gov.bc.ca>
2024-10-15 16:35:03 +0000

    - Updated sync secrets GHA to build the image and push to GCHR - Added shells script that will pull secrets and push to AWS - Added sync_secrets yaml for the CronJob template for Openshift

---
+++ docker/openshift/sync_secrets.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+# Vault details
+VAULT_SECRET_ENV="${VAULT_SECRET_ENV}"
+LOCAL_SECRET_PATH="${LOCAL_SECRET_PATH}"
+
+#export HTTP_PROXY=http://swpxkam.gov.bc.ca:8080
+#export HTTPS_PROXY=http://swpxkam.gov.bc.ca:8080
+#export NO_PROXY=.cluster.local,.svc,10.91.0.0/16,172.30.0.0/16,127.0.0.1,localhost,.gov.bc.ca
+
+aws_secret_format="jasper-X-secret-$VAULT_SECRET_ENV"
+secret_keys="\
+  aspnet_core \
+  auth \
+  database \
+  file_services_client \
+  keycloak \
+  location_services_client \
+  lookup_services_client \
+  misc \
+  request \
+  splunk \
+  user_services_client"
+
+# Iterate on each key to get the value from Vault and save in AWS secrets manager
+for key in $secret_keys; do
+  value=$(jq -r ".${VAULT_SECRET_ENV}_$key" "$LOCAL_SECRET_PATH")
+
+  echo "=========================="
+  echo "KEY: $key"
+  echo "VALUE: $value"
+
+  sanitizedKey=$(echo "$key" | sed "s/_/-/g")
+  secret_name=$(echo "$aws_secret_format" | sed "s/X/$sanitizedKey/")
+  echo "Uploading $secret_name"
+  aws secretsmanager put-secret-value \
+    --secret-id $secret_name \
+    --secret-string $value
+done
+
+if [ $? -eq 0 ]; then
+  echo "Secrets synced successfully from Vault to AWS Secrets Manager."
+else
+  echo "Failed to sync secrets."
+fi

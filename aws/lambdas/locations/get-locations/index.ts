50fa6f9b1bd05fa4350f5c839093c09c46d855d6
Ronaldo Macapobre <rmacapob@gov.bc.ca>
2024-11-13 06:53:29 +0000

    - Further reduce test data for get-rooms and get-locations

--- aws/lambdas/locations/get-locations/index.ts
+++ aws/lambdas/locations/get-locations/index.ts
@@ -22,62 +22,6 @@ export const handler = async (
         shortDesc: "1051",
         longDesc: "Duncan Law Courts",
         flex: "Y"
-      },
-      {
-        codeType: "COURT_LOCATIONS",
-        code: "10232.0001",
-        shortDesc: "1061",
-        longDesc: "Ganges Provincial Court",
-        flex: "Y"
-      },
-      {
-        codeType: "COURT_LOCATIONS",
-        code: "10233.0001",
-        shortDesc: "1071",
-        longDesc: "Gold River Provincial Court",
-        flex: "Y"
-      },
-      {
-        codeType: "COURT_LOCATIONS",
-        code: "10234.0001",
-        shortDesc: "1111",
-        longDesc: "Parksville Provincial Court",
-        flex: "Y"
-      },
-      {
-        codeType: "COURT_LOCATIONS",
-        code: "10235.0001",
-        shortDesc: "1121",
-        longDesc: "Port Alberni Law Courts",
-        flex: "Y"
-      },
-      {
-        codeType: "COURT_LOCATIONS",
-        code: "10236.0001",
-        shortDesc: "1141",
-        longDesc: "Port Hardy Law Courts",
-        flex: "Y"
-      },
-      {
-        codeType: "COURT_LOCATIONS",
-        code: "10237.0001",
-        shortDesc: "1145",
-        longDesc: "Powell River Law Courts",
-        flex: "Y"
-      },
-      {
-        codeType: "COURT_LOCATIONS",
-        code: "10238.0001",
-        shortDesc: "1151",
-        longDesc: "Sidney Provincial Court",
-        flex: "Y"
-      },
-      {
-        codeType: "COURT_LOCATIONS",
-        code: "10239.0001",
-        shortDesc: "1171",
-        longDesc: "Tahsis Provincial Court",
-        flex: "Y"
       }
     ])
   }
--- aws/lambdas/locations/get-rooms/index.ts
+++ aws/lambdas/locations/get-rooms/index.ts
@@ -22,62 +22,6 @@ export const handler = async (
         shortDesc: "CRT",
         longDesc: "1031-001",
         flex: "1031"
-      },
-      {
-        codeType: "COURT_ROOMS",
-        code: "001",
-        shortDesc: "CRT",
-        longDesc: "1051-001",
-        flex: "1051"
-      },
-      {
-        codeType: "COURT_ROOMS",
-        code: "001",
-        shortDesc: "CRT",
-        longDesc: "1201-001",
-        flex: "1201"
-      },
-      {
-        codeType: "COURT_ROOMS",
-        code: "001",
-        shortDesc: "CRT",
-        longDesc: "2007-001",
-        flex: "2007"
-      },
-      {
-        codeType: "COURT_ROOMS",
-        code: "001",
-        shortDesc: "CRT",
-        longDesc: "2031-001",
-        flex: "2031"
-      },
-      {
-        codeType: "COURT_ROOMS",
-        code: "001",
-        shortDesc: "CRT",
-        longDesc: "2040-001",
-        flex: "2040"
-      },
-      {
-        codeType: "COURT_ROOMS",
-        code: "001",
-        shortDesc: "CRT",
-        longDesc: "2045-001",
-        flex: "2045"
-      },
-      {
-        codeType: "COURT_ROOMS",
-        code: "001",
-        shortDesc: "CRT",
-        longDesc: "2048-001",
-        flex: "2048"
-      },
-      {
-        codeType: "COURT_ROOMS",
-        code: "001",
-        shortDesc: "CRT",
-        longDesc: "3531-001",
-        flex: "3531"
       }
     ])
   }

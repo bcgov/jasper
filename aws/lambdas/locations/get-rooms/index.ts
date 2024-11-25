081f4985ed08a1f37134a58fe75961afefd5100b
Ronaldo Macapobre <rmacapob@gov.bc.ca>
2024-11-25 12:41:19 -0800

    Test call to WSGW

--- aws/lambdas/locations/get-rooms/index.ts
+++ aws/lambdas/locations/get-rooms/index.ts
@@ -1,28 +1,40 @@
-import { APIGatewayEvent, APIGatewayProxyResult, Context } from "aws-lambda"
+import { APIGatewayEvent, APIGatewayProxyResult, Context } from "aws-lambda";
+import HttpService from "../../../services/httpService";
 
 export const handler = async (
   event: APIGatewayEvent,
   context: Context
 ): Promise<APIGatewayProxyResult> => {
-  console.log(event, context)
+  console.log(event, context);
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify([
-      {
-        codeType: "COURT_ROOMS",
-        code: "00",
-        shortDesc: "CRT",
-        longDesc: "NIDD-00",
-        flex: "NIDD"
-      },
-      {
-        codeType: "COURT_ROOMS",
-        code: "001",
-        shortDesc: "CRT",
-        longDesc: "1031-001",
-        flex: "1031"
-      }
-    ])
+  try {
+    const http = new HttpService();
+    await http.init("https://wsgw.dev.jag.gov.bc.ca/dev");
+    const result = await http.get("/TestService");
+
+    console.log(result);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify([
+        {
+          codeType: "COURT_ROOMS",
+          code: "00",
+          shortDesc: "CRT",
+          longDesc: "NIDD-00",
+          flex: "NIDD",
+        },
+        {
+          codeType: "COURT_ROOMS",
+          code: "001",
+          shortDesc: "CRT",
+          longDesc: "1031-001",
+          flex: "1031",
+        },
+      ]),
+    };
+  } catch (error) {
+    console.error(error);
+    throw error;
   }
-}
+};

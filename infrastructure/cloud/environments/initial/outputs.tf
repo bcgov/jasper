146e574c13df97c64fbdf8ddda238eafc65e1b48
Ronaldo Macapobre <rmacapob@gov.bc.ca>
2024-12-11 00:50:39 +0000

    test

--- infrastructure/cloud/environments/initial/outputs.tf
+++ infrastructure/cloud/environments/initial/outputs.tf
@@ -1,7 +1,7 @@
 output "app_ecr_name" {
-  value = module.app_ecr.aws_ecr_repository.ecr_repository.name
+  value = module.app_ecr.ecr_name
 }
 
 output "lambda_ecr_name" {
-  value = module.lambda_ecr.aws_ecr_repository.ecr_repository.name
+  value = module.lambda_ecr.ecr_name
 }
--- infrastructure/cloud/modules/ECR/outputs.tf
+++ infrastructure/cloud/modules/ECR/outputs.tf
@@ -1,3 +1,7 @@
 output "ecs_cluster_arn" {
   value = aws_ecr_repository.ecr_repository.arn
 }
+
+output "ecr_name" {
+  value = aws_ecr_repository.ecr_repository.name
+}

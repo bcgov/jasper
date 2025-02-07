7614dcc9829d24d097a437cff74fbfc43286429e
Ronaldo Macapobre <rmacapob@gov.bc.ca>
2025-02-07 22:50:33 +0000

    Force apigw redeployment when apigw settings is updated

--- infrastructure/cloud/modules/APIGateway/main.tf
+++ infrastructure/cloud/modules/APIGateway/main.tf
@@ -10,7 +10,7 @@ resource "aws_api_gateway_deployment" "apigw_deployment" {
   rest_api_id = aws_api_gateway_rest_api.apigw.id
 
   triggers = {
-    redeployment = sha1(jsonencode(aws_api_gateway_rest_api.apigw.body))
+    redeployment = sha1(jsonencode(aws_api_gateway_rest_api.apigw))
   }
 
   lifecycle {

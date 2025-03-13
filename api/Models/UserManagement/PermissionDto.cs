ece8fcffeeb656478ea36723d1631533a24aa9b6
Ronaldo Macapobre <rmacapob@gov.bc.ca>
2025-03-13 22:39:56 +0000

    Mark Permission.IsActive as nullable to fix SonarCloud issue.

--- api/Models/UserManagement/PermissionDto.cs
+++ api/Models/UserManagement/PermissionDto.cs
@@ -5,5 +5,5 @@ public class PermissionDto : UserManagementDto
     public string Name { get; set; }
     public string Code { get; set; }
     public string Description { get; set; }
-    public bool IsActive { get; set; }
+    public bool? IsActive { get; set; }
 }

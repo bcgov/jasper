f7961824a70646f6eaa1894cf359f3f909326621
Ronaldo Macapobre <rmacapob@gov.bc.ca>
2025-03-26 17:12:45 +0000

    - Import ReportServicesClient to be able to call PCSS' generate court list report - Integrate with CourtListService to be able to call from CourtListController - Expose generate-report endpoint to be called by FE - Added validators and unit tests

---
+++ api/Validators/CourtList/CourtListReportRequestValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using Scv.Api.Models.CourtList;
+
+namespace Scv.Api.Validators.CourtList;
+
+public class CourtListReportRequestValidator : AbstractValidator<CourtListReportRequest>
+{
+    public CourtListReportRequestValidator()
+    {
+        RuleFor(x => x.CourtDivision)
+           .NotEmpty().WithMessage("Division is required.");
+
+        RuleFor(x => x.Date)
+            .NotEmpty().WithMessage("Date is required.");
+
+        RuleFor(x => x.LocationId)
+            .GreaterThan(0).WithMessage("Location ID is invalid.");
+
+        RuleFor(x => x.CourtClass)
+            .NotEmpty().WithMessage("Class is required.");
+    }
+}

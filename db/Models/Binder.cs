1475f8ee519970e1a2e0dcf174cfa52e13a499cf
Ronaldo Macapobre <rmacapob@gov.bc.ca>
2025-06-04 11:39:18 -0700

    Update Binder.Labels as list of KVPairs

--- db/Models/Binder.cs
+++ db/Models/Binder.cs
@@ -6,7 +6,7 @@ namespace Scv.Db.Models;
 [Collection("binders")]
 public class Binder : EntityBase
 {
-    public KeyValuePair<Label, object> Labels { get; set; }
+    public List<KeyValuePair<Label, object>> Labels { get; set; }
     public List<Tag> Tags { get; set; } = [];
     public List<BinderDocument> Documents { get; set; } = [];
 }

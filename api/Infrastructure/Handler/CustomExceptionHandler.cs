18c3d6a2c41d5e44ef58c7cbf73e7ab350201a6a
Ronaldo Macapobre <rmacapob@gov.bc.ca>
2025-10-14 13:15:36 -0700

    Backend - Add custom exception handling to catch and log unhandled exceptions - Remove unnecessary pinging of Mongo database. - Add try catch to Program.cs to be able to log exceptions before the app exits - Update MigrationAndSeedService to properly async and await to avoid thread locks - Move Hangfire registration towards the and of app Startup to ensure all dependencies has been registered
    Infrastructure
    - Add EFS module to easily attach it to services (e.g. ECS, EC2)

---
+++ api/Infrastructure/Handler/CustomExceptionHandler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace Scv.Api.Infrastructure.Handler;
+
+public class CustomExceptionHandler(ILogger<CustomExceptionHandler> logger) : IExceptionHandler
+{
+    public ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+    {
+        var exceptionMessage = exception.Message;
+        logger.LogError(
+            "Error Message: {exceptionMessage}, Time of occurrence {time}",
+            exceptionMessage, DateTime.UtcNow);
+        return ValueTask.FromResult(false);
+    }
+}

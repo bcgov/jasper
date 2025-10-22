1d85ed4a34dfa6c09f0d670440c049d9c382c3df
JTraill <josh.traill@quartech.com>
2025-10-22 11:59:14 -0700

    refactor: log who meta env

--- web/src/components/documents/FileViewer.vue
+++ web/src/components/documents/FileViewer.vue
@@ -72,7 +72,7 @@
     container: '.pdf-container',
     licenseKey: import.meta.env.VITE_NUTRIENT_FE_LICENSE_KEY,
   };
-  console.log(import.meta.env.VITE_NUTRIENT_FE_LICENSE_KEY);
+  console.log(import.meta.env);
   console.log(process.env.VITE_NUTRIENT_FE_LICENSE_KEY);
   const loadNutrient = async () => {
     loading.value = true;

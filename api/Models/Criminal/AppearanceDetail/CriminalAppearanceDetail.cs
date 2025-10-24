50442c5b825fe8f267550599c36966cb23b6aa10
devinleighsmith <devinleighsmith@gmail.com>
2025-10-24 16:47:15 -0700

    update location of files with db/jc interface dependencies.

---
+++ api/Models/Criminal/AppearanceDetail/CriminalAppearanceDetail.cs
@@ -0,0 +1,41 @@
+﻿using JCCommon.Clients.FileServices;
+using Scv.Api.Helpers;
+using Scv.Models.Criminal.AppearanceDetail;
+using Scv.Models.Criminal.Detail;
+using System.Collections.Generic;
+
+
+namespace Scv.Api.Models.Criminal.AppearanceDetail;
+
+public class CriminalAppearanceDetail
+{
+    public string JustinNo { get; set; }
+    public string AppearanceId { get; set; }
+    public string PartId { get; set; }
+    public string AgencyId { get; set; }
+    public string ProfSeqNo { get; set; }
+    public string CourtRoomCd { get; set; }
+    public string FileNumberTxt { get; set; }
+    public string AppearanceDt { get; set; }
+    public JustinCounsel JustinCounsel { get; set; }
+    public CriminalAccused Accused { get; set; }
+    public Prosecutor Prosecutor { get; set; }
+    public CriminalAdjudicator Adjudicator { get; set; }
+    public string JudgesRecommendation { get; set; }
+    public string AppearanceNote { get; set; }
+    /// <summary>
+    /// Extended CriminalAppearanceCount object.
+    /// </summary>
+    public ICollection<CriminalCharges> Charges { get; set; }
+
+    /// <summary>
+    /// Extended CriminalAppearanceMethod object.
+    /// </summary>
+    public ICollection<Scv.Models.Criminal.AppearanceDetail.CriminalAppearanceMethod> AppearanceMethods { get; set; }
+    public string EstimatedTimeHour { get; set; }
+    public string EstimatedTimeMin { get; set; }
+    public IEnumerable<CriminalDocument> Documents { get; set; }
+    public IEnumerable<CriminalDocument> KeyDocuments => KeyDocumentResolver.GetCriminalKeyDocuments(Documents);
+    
+    public CriminalFileDetailResponseCourtLevelCd CourtLevelCd { get; set; }
+}

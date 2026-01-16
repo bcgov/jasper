655e728066181fab94a8e2b2be049e2fba89b539
Ronaldo Macapobre <ronaldo.macapobre@quartech.com>
2026-01-16 11:20:19 -0800

    JASPER-715: Digital Signing: Create the process for emailing information about orders that have been received. (#774)
    * - Add sending of email functionality
    - Add EmailTemplate seeder and OrderReceived template
    - Add Scriban for text templating
    
    * Update namespace
    
    * Remove sensitive logging of email
    
    * Fix failing unit test
    
    * Update order received template
    
    ---------
    
    Co-authored-by: Ronaldo Macapobre <rmacapob@gov.bc.ca>

---
+++ db/Models/EmailTemplate.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using MongoDB.EntityFrameworkCore;
+using Scv.Db.Contants;
+
+namespace Scv.Db.Models;
+
+[Collection(CollectionNameConstants.EMAIL_TEMPLATES)]
+
+public class EmailTemplate : EntityBase
+{
+    public const string ORDER_RECEIVED = "Order Received";
+
+    public static readonly List<EmailTemplate> ALL_EMAIL_TEMPLATES =
+    [
+        new EmailTemplate
+        {
+            TemplateName = ORDER_RECEIVED,
+            Subject = @"Order Received for {{ case_file_number }}",
+            Body = @"Dear Judge <b>{{ last_name }}</b>,<br /><br />
+                     You are receiving this email because you have received an order Case File <b>{{ case_file_number }}</b>. Please login to <a href='{{ jasper_url }}'>JASPER</a> to confirm and verify.<br /><br />
+                     Other pending orders for your review:<br />
+                     <ul id='orders'>
+                        {{ for order in orders }}
+                        <li><a href='{{ order.order_url }}'>{{ order.case_file_number }}</a></li>
+                        {{ end }}
+                     </ul>
+                     Regards,<br />
+                     JASPER Support Team"
+        },
+    ];
+
+    public string TemplateName { get; set; }
+    public string Subject { get; set; }
+    public string Body { get; set; }
+}
---
+++ db/Seeders/EmailTemplateSeeder.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Scv.Db.Contexts;
+using Scv.Db.Models;
+
+namespace Scv.Db.Seeders;
+
+public class EmailTemplateSeeder(ILogger<EmailTemplateSeeder> logger) : SeederBase<JasperDbContext>(logger)
+{
+    public override int Order => 6;
+
+    protected override async Task ExecuteAsync(JasperDbContext context)
+    {
+        var allDefaultTemplates = EmailTemplate.ALL_EMAIL_TEMPLATES;
+
+        this.Logger.LogInformation("\tAdding email templates...");
+        foreach (var template in allDefaultTemplates)
+        {
+            var t = await context.EmailTemplates.FirstOrDefaultAsync(et => et.TemplateName == template.TemplateName);
+            if (t == null)
+            {
+                await context.EmailTemplates.AddAsync(template);
+            }
+            else
+            {
+                this.Logger.LogInformation("{TemplateName} email template already exists. Skipping...", template.TemplateName);
+            }
+        }
+
+        await context.SaveChangesAsync();
+    }
+}

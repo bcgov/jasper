64e59a7f9edc3c4afd06878d6dcdbf8c4e1e42d4
JTraill <josh.traill@quartech.com>
2026-01-22 13:37:39 -0800

    feat: default orderstatus to unapproved

--- api/Models/Order/OrderReview.cs
+++ api/Models/Order/OrderReview.cs
@@ -7,7 +7,7 @@ namespace Scv.Api.Models.Order;
 [JsonConverter(typeof(FlexibleNamingJsonConverter<OrderReview>))]
 public class OrderReview
 {
-    public OrderStatus Status { get; set; }
+    public OrderStatus Status { get; set; } = OrderStatus.Unapproved;
     public bool Signed { get; set; } = false;
     public string Comments { get; set; }
     public string DocumentData { get; set; }

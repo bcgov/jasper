320fbbe9d35f92c62fb7d3dbf3d482cc902aafca
Ronaldo Macapobre <rmacapob@gov.bc.ca>
2026-01-22 19:28:17 +0000

    Updates to be able to deploy Terraform code to SEA envs

--- infrastructure/cloud/modules/Subnets/outputs.tf
+++ infrastructure/cloud/modules/Subnets/outputs.tf
@@ -1,20 +1,41 @@
+# output "web_subnets_ids" {
+#   value = data.aws_subnets.web.ids
+
+# }
+
+# output "app_subnets_ids" {
+#   value = data.aws_subnets.app.ids
+# }
+
+# output "data_subnets_ids" {
+#   value = data.aws_subnets.data.ids
+# }
+
+# output "all_subnet_ids" {
+#   value = concat(
+#     data.aws_subnets.web.ids,
+#     data.aws_subnets.app.ids,
+#     data.aws_subnets.data.ids
+#   )
+# }
+
 output "web_subnets_ids" {
-  value = data.aws_subnets.web.ids
-  
+  value = [for subnet in local.web_subnets : subnet.id]
+
 }
 
 output "app_subnets_ids" {
-  value = data.aws_subnets.app.ids
+  value = [for subnet in local.app_subnets : subnet.id]
 }
 
 output "data_subnets_ids" {
-  value = data.aws_subnets.data.ids
+  value = [for subnet in local.data_subnets : subnet.id]
 }
 
 output "all_subnet_ids" {
   value = concat(
-    data.aws_subnets.web.ids,
-    data.aws_subnets.app.ids,
-    data.aws_subnets.data.ids
+    [for subnet in local.web_subnets : subnet.id],
+    [for subnet in local.app_subnets : subnet.id],
+    [for subnet in local.data_subnets : subnet.id]
   )
 }

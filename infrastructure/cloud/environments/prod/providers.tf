f884765db82cd9bfee6f6d1c87e7eb7f716e95d3
Ronaldo Macapobre <rmacapob@gov.bc.ca>
2026-02-10 17:24:11 +0000

    - Specify 3h duration window for authorizer key rotation - Update TF version and aws

--- infrastructure/cloud/environments/prod/providers.tf
+++ infrastructure/cloud/environments/prod/providers.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version = "~> 1.13.1"
+  required_version = "~> 1.14.1"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.98.0"
+      version = "~> 6.0.0"
     }
 
     tls = {

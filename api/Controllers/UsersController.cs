ac6d36918af1b0bf1608802595477f88049d748e
Ronaldo Macapobre <ronaldo.macapobre@gov.bc.ca>
2026-08-07 18:01:54 +0000

    Refactor to remove duplicate code

--- api/Controllers/UsersController.cs
+++ api/Controllers/UsersController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using FluentValidation;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Scv.Api.Infrastructure.Authorization;
